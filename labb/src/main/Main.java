04ac3d0f23896cff62131ea236ea5c123e98beb7
Timmy0012 <timmyhjelm@live.se>
2025-10-16 12:21:12 +0200

    Testar

--- labb/src/main/Main.java
+++ labb/src/main/Main.java
@@ -1,10 +1,34 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		
+		while (choice != 1 && choice != 2) {
+			System.out.println("Choose an option: ");
+			System.out.println("1 - Login");
+			System.out.println("2 - Continue as Guest");
+			System.out.println("Select option 1 or 2 to Continue");
+			
+			choice = scan.nextInt();
+			if (choice != 1 && choice != 2) {
+				System.out.println("Invalid option, try again!");
+			}
+		}
+		boolean Logged_In = false;
+		if (choice == 1) {
+			Logged_In = true;
+		}
+		else {
+			//Call Guest Class.
+		}
+		
+		System.out.println("Enter your username: ");
+		
+		
 	}
 
 }
