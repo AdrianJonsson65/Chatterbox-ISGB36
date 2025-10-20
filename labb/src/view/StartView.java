7edc8d4ec6e26a3cb4006b3b27506d5eebb30026
Timmy0012 <timmyhjelm@live.se>
2025-10-20 12:52:34 +0200

    aaa

--- labb/src/view/StartView.java
+++ labb/src/view/StartView.java
@@ -1,5 +1,29 @@
 package view;
-
+import java.util.Scanner;
+//View for the start interface which allows user login or to continue as a guest depending on the choice made.
 public class StartView {
 
+	public StartView(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		
+		while (choice != 1 && choice != 2) {
+			System.out.println("Choose an option: ");
+			System.out.println("1 - Login");
+			System.out.println("2 - Continue as Guest");
+			System.out.println("Select option 1 or 2 to Continue");
+			
+			choice = scan.nextInt();
+			switch (choice) {
+			case 1:
+				//Login.LoginView();
+				break;
+			case 2:
+				//Message.viewAll(); 
+				break;
+			default:
+				System.out.println("Please enter a valid option!");
+			}
+		}	
+	}
 }
